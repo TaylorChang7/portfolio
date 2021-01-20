ed5a6414e3faf53a9622a1f098cdd846f43ced42
TaylorChang7 <hct9at@virginia.edu>
2021-01-20 14:57:23 -0500

    added requirements.txt

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,10 +119,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'staticfiles')
 ]
-django_heroku.settings(locals())
+
+"""
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except ImportError:
+    found = False
+
+
+"""
